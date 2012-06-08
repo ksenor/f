9dfc9879ec68a4de6d582ba0fe30f2b55f9ac0f6
unknown <@freeworld.(none)>
2012-06-08 22:32:44 +0400

    first commit just for example - we are should to use pyevolve library and try to make trees of functions, which are should to prettity our input forex data to np.array() objects with possibility to using by net.train(inp, epochs, show) where is inp is our generated by this one array, and net is neurolab.net.new(dimension_list, count_neurons)

--- new  2.py
+++ new  2.py
@@ -1,4 +1,4 @@
-﻿# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 """ 
 Example of use competitive layer(newc)
 ======================================
@@ -9,14 +9,17 @@ import numpy as np
 import neurolab as nl
 import numpy.random as rand
 
-centr = np.array([[0.2, 0.2], [0.4, 0.4], [0.7, 0.3], [0.2, 0.5]])
-rand_norm = 0.05 * rand.randn(100, 4, 2)
+centr = np.array([[0.2, 0.2, 0.2], 
+                  [0.4, 0.4, 0.4], 
+                  [0.7, 0.3, 0.6], 
+                  [0.3, 0.2, 0.5]])
+rand_norm = 0.05 * rand.randn(100, 4, 3)
 inp = np.array([centr + r for r in rand_norm])
-inp.shape = (100 * 4, 2)
-rand.shuffle(inp) 
+inp.shape = (100 * 4, 3)
+rand.shuffle(inp)
 
 # Create net with 2 inputs and 4 neurons
-net = nl.net.newc([[0.0, 1.0],[0.0, 1.0]], 4)
+net = nl.net.newc([[0.0, 1.0],[0.0, 1.0], [0.0, 1.0]], 4)
 # train with rule: Conscience Winner Take All algoritm (CWTA)
 error = net.train(inp, epochs=200, show=20)
 
@@ -27,6 +30,8 @@ pl.subplot(211)
 pl.plot(error)
 pl.xlabel('Epoch number')
 pl.ylabel('error (default MAE)')
+import pdb
+pdb.set_trace()
 w = net.layers[0].np['w']
 
 pl.subplot(212)
