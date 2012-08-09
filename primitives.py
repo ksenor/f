d2b6afe3009fc7b67a9e249e65fecc33b34faf03
Aleksander Krivosheev <KrivosheevAA@GDEETOTDOM.RU>
2012-08-09 19:01:55 +0400

    так, с примитивами пока закончили

--- primitives.py
+++ primitives.py
@@ -2,23 +2,16 @@
 
 import math
 
-# примитивы в математике
+# очень примитивы
 def add(a, b): return a+b # сложение
 def sub(a, b): return a-b # вычитание
 def mul(a, b): return a*b # умножение
 def div(a,b): return a/b # деление
 def sqrt(a): return math.sqrt(abs(a)) # корень
 def exponentiation(x, n): return x**n # возведение в степень
-    
-    
-# операции над всем списком фин.данных
-def cng_vec_pos(data_list, pos1, pos2):
-    '''
-    меняет местами векторы во входящем list'е данных
-    '''
-    data_list[pos1], data_list[pos2] \
-    = data_list[pos2], data_list[pos1]
-
+def first_elem(data_list): return data_list[0] # первый элемент
+def last_elem(data_list): return data_list[-1] # последний элемент
+def any_elem(data_list, pos): return data_list[pos] # произвольный элемент
 
 # операции над векторами
 def vec_sum(vec1, vec2): # сложение векторов
@@ -33,4 +26,39 @@ def vec_sub(vec1, vec2): # вычитание векторов
         elem -= vec2[pos]
         vec_result.append(elem)
     return vec_result
-# 
+def vec_mul(vec1, vec2): # умножить
+    vec_result = []
+    for pos, elem in enumerate(vec1):
+        elem *= vec2[pos]
+        vec_result.append(elem)
+    return vec_result
+def vec_div(vec1, vec2): # делить
+    vec_result = []
+    for pos, elem in enumerate(vec1):
+        elem /= vec2[pos]
+        vec_result.append(elem)
+    return vec_result
+def vec_sqrt(vec): # извлечь корень каждого значения
+    for pos, elem in enumerate(vec):
+        vec[pos] = math.sqrt(elem)
+    return vec
+def vec_exponentiation(vec, n): # возвести в степень
+    for pos, elem in enumerate(vec):
+        vec[pos] = elem**n
+    return vec
+
+
+# операции над всем списком фин.данных
+def get_couple_vec(data_list, elem1pos, elem2pos):
+    return [data_list[elem1pos], data_list[elem2pos]]
+def cng_vec_pos(data_list, pos1, pos2):
+    '''
+    меняет местами векторы во входящем list'е данных
+    '''
+    data_list[pos1], data_list[pos2] \
+    = data_list[pos2], data_list[pos1]
+    return data_list
+def sum_vec_to1(vec1, vec2): # суммируем векторы в один
+    return vec_sum(vec1, vec2)
+def div_vec_to1(vec1, vec2): # получаем отношение одного вектора к другому
+    return vec_div(vec1, vec2)
--- test_primitives.py
+++ test_primitives.py
@@ -1,4 +1,4 @@
-﻿# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
 import unittest as ut
 
@@ -13,7 +13,7 @@ class DataLoaderTester(ut.TestCase):
         
         self.primitive = p
     
-    def test_basic_math(self):
+    def test_basic_primitives(self):
         '''
         все базовые математические операции над 
         одним, или несколькими аргументами
@@ -30,27 +30,22 @@ class DataLoaderTester(ut.TestCase):
         self.assertEqual(self.primitive.sqrt(100), 10)
         # степень
         self.assertEqual(self.primitive.exponentiation(10, 2), 100)
-
-    def test_list_data_opers(self):
-        # меняем местами векторы
-        self.assertEqual(self.primitive.cng_vec_pos(
-                            [[1,2], [3,4]]), [[3,4], [1,2]])
-        # складываем 2 вектора в 1
-        self.assertEqual(self.primitive.add_vec_to1(
-                            [[1,2], [3,4]], [[4,6]]))
-        # делим один вектор на другой 
-        self.assertEqual(self.primitive.div_vec_to1(
-                            [[3,49,100],[3,7,10]], [[1,7,10]]))
+        # первый из ..
+        self.assertEqual(self.primitive.first_elem([1,2,3]), 1)
+        # последний из ..
+        self.assertEqual(self.primitive.last_elem([1,2,3]), 3)
+        # заданный элемент из множества
+        self.assertEqual(self.primitive.any_elem([1,2,3], 1), 2)
 
     def test_vec_opers(self):
         '''
         все базовые математические операции над 
-        одним, или несколькими аргументами
+        одним, или несколькими векторами
         '''
         # плюс
-        self.assertEqual(self.primitive.vec_add([1,2], [4,3]), [5,5])
+        self.assertEqual(self.primitive.vec_sum([1,2], [4,3]), [5,5])
         # минус
-        self.assertEqual(self.primitive.vec_div([1,2], [1,2]), [0,0])
+        self.assertEqual(self.primitive.vec_sub([1,2], [1,2]), [0,0])
         # умножить
         self.assertEqual(self.primitive.vec_mul([1,2], [2,3]), [2,6])
         # делить
@@ -58,7 +53,23 @@ class DataLoaderTester(ut.TestCase):
         # корень каждого из значений
         self.assertEqual(self.primitive.vec_sqrt([100, 49]), [10, 7])
         # степень - = -
-        self.assertEqual(self.primitive.exponentiation([10, 3], 2), [100, 6])
+        self.assertEqual(self.primitive.vec_exponentiation(
+                            [10, 3], 2), [100, 9])
+
+    def test_list_data_opers(self):
+        # получаем из всего списка пару векторов
+        self.assertEqual(self.primitive.get_couple_vec(
+                            [[1,2,3], [3,1,2], [5,1,2]], 0, 2),
+                            [[1,2,3], [5,1,2]])
+        # меняем местами векторы
+        self.assertEqual(self.primitive.cng_vec_pos(
+                            [[1,2], [3,4]], 0, 1), [[3,4], [1,2]])
+        # складываем 2 вектора в 1
+        self.assertEqual(self.primitive.sum_vec_to1(
+                            [1,2], [3,4]), [4,6])
+        # делим один вектор на другой 
+        self.assertEqual(self.primitive.div_vec_to1(
+                            [3,49,100],[3,7,10]), [1,7,10])
         
         
 if __name__ == '__main__':
