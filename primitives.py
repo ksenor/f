1a31f6896220a5849068cdf22261e9b6666b8bc0
Aleksander Krivosheev <KrivosheevAA@GDEETOTDOM.RU>
2012-08-09 17:57:25 +0400

    промежуточные результаты - тесты написаны, осталось написать примитивы

---
+++ primitives.py
@@ -0,0 +1,36 @@
+﻿# -*- coding: utf-8 -*-
+
+import math
+
+# примитивы в математике
+def add(a, b): return a+b # сложение
+def sub(a, b): return a-b # вычитание
+def mul(a, b): return a*b # умножение
+def div(a,b): return a/b # деление
+def sqrt(a): return math.sqrt(abs(a)) # корень
+def exponentiation(x, n): return x**n # возведение в степень
+    
+    
+# операции над всем списком фин.данных
+def cng_vec_pos(data_list, pos1, pos2):
+    '''
+    меняет местами векторы во входящем list'е данных
+    '''
+    data_list[pos1], data_list[pos2] \
+    = data_list[pos2], data_list[pos1]
+
+
+# операции над векторами
+def vec_sum(vec1, vec2): # сложение векторов
+    vec_result = []
+    for pos, elem in enumerate(vec1):
+        elem += vec2[pos]
+        vec_result.append(elem)
+    return vec_result
+def vec_sub(vec1, vec2): # вычитание векторов
+    vec_result = []
+    for pos, elem in enumerate(vec1):
+        elem -= vec2[pos]
+        vec_result.append(elem)
+    return vec_result
+# 
---
+++ test_primitives.py
@@ -0,0 +1,65 @@
+﻿# -*- coding: utf-8 -*-
+
+import unittest as ut
+
+import primitives as p
+
+class DataLoaderTester(ut.TestCase):
+
+    def setUp(self):
+        '''
+        тестирование правильной работы примитивов
+        '''
+        
+        self.primitive = p
+    
+    def test_basic_math(self):
+        '''
+        все базовые математические операции над 
+        одним, или несколькими аргументами
+        '''
+        # плюс
+        self.assertEqual(self.primitive.add(1,2), 3)
+        # минус
+        self.assertEqual(self.primitive.sub(3,1), 2)
+        # умножить
+        self.assertEqual(self.primitive.mul(2,3), 6)
+        # делить
+        self.assertEqual(self.primitive.div(6,3), 2)
+        # корень
+        self.assertEqual(self.primitive.sqrt(100), 10)
+        # степень
+        self.assertEqual(self.primitive.exponentiation(10, 2), 100)
+
+    def test_list_data_opers(self):
+        # меняем местами векторы
+        self.assertEqual(self.primitive.cng_vec_pos(
+                            [[1,2], [3,4]]), [[3,4], [1,2]])
+        # складываем 2 вектора в 1
+        self.assertEqual(self.primitive.add_vec_to1(
+                            [[1,2], [3,4]], [[4,6]]))
+        # делим один вектор на другой 
+        self.assertEqual(self.primitive.div_vec_to1(
+                            [[3,49,100],[3,7,10]], [[1,7,10]]))
+
+    def test_vec_opers(self):
+        '''
+        все базовые математические операции над 
+        одним, или несколькими аргументами
+        '''
+        # плюс
+        self.assertEqual(self.primitive.vec_add([1,2], [4,3]), [5,5])
+        # минус
+        self.assertEqual(self.primitive.vec_div([1,2], [1,2]), [0,0])
+        # умножить
+        self.assertEqual(self.primitive.vec_mul([1,2], [2,3]), [2,6])
+        # делить
+        self.assertEqual(self.primitive.vec_div([6,3], [2,3]), [3,1])
+        # корень каждого из значений
+        self.assertEqual(self.primitive.vec_sqrt([100, 49]), [10, 7])
+        # степень - = -
+        self.assertEqual(self.primitive.exponentiation([10, 3], 2), [100, 6])
+        
+        
+if __name__ == '__main__':
+    ut.main()
