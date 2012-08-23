84ba94dee28eea8099c71a4da6afb8ff70364049
Aleksander Krivosheev <KrivosheevAA@GDEETOTDOM.RU>
2012-08-23 23:24:29 +0400

    че, пишем, пишем...

--- primitives.py
+++ primitives.py
@@ -3,62 +3,104 @@
 import math
 
 # очень примитивы
-def add(a, b): return a+b # сложение
-def sub(a, b): return a-b # вычитание
-def mul(a, b): return a*b # умножение
-def div(a,b): return a/b # деление
-def sqrt(a): return math.sqrt(abs(a)) # корень
-def exponentiation(x, n): return x**n # возведение в степень
-def first_elem(data_list): return data_list[0] # первый элемент
-def last_elem(data_list): return data_list[-1] # последний элемент
-def any_elem(data_list, pos): return data_list[pos] # произвольный элемент
+def a_add(a, b): return a+b # сложение
+def a_sub(a, b): return a-b # вычитание
+def a_mul(a, b): return a*b # умножение
+def a_div(a,b): return a/b # деление
+def a_sqrt(a): return math.sqrt(abs(a)) # корень
+def a_exponentiation(x, n): return x**n # возведение в степень
+def a_first_elem(data_list): return data_list[0] # первый элемент
+def a_last_elem(data_list): return data_list[-1] # последний элемент
+def a_any_elem(data_list, pos): return data_list[pos] # произвольный элемент
 
 # операции над векторами
-def vec_sum(vec1, vec2): # сложение векторов
+def a_vec_sum(vec1, vec2): # сложение векторов
     vec_result = []
     for pos, elem in enumerate(vec1):
         elem += vec2[pos]
         vec_result.append(elem)
     return vec_result
-def vec_sub(vec1, vec2): # вычитание векторов
+def a_vec_sub(vec1, vec2): # вычитание векторов
     vec_result = []
     for pos, elem in enumerate(vec1):
         elem -= vec2[pos]
         vec_result.append(elem)
     return vec_result
-def vec_mul(vec1, vec2): # умножить
+def a_vec_mul(vec1, vec2): # умножить
     vec_result = []
     for pos, elem in enumerate(vec1):
         elem *= vec2[pos]
         vec_result.append(elem)
     return vec_result
-def vec_div(vec1, vec2): # делить
+def a_vec_div(vec1, vec2): # делить
     vec_result = []
     for pos, elem in enumerate(vec1):
         elem /= vec2[pos]
         vec_result.append(elem)
     return vec_result
-def vec_sqrt(vec): # извлечь корень каждого значения
+def a_vec_sqrt(vec): # извлечь корень каждого значения
     for pos, elem in enumerate(vec):
         vec[pos] = math.sqrt(elem)
     return vec
-def vec_exponentiation(vec, n): # возвести в степень
+def a_vec_exponentiation(vec, n): # возвести в степень
     for pos, elem in enumerate(vec):
         vec[pos] = elem**n
     return vec
 
 
 # операции над всем списком фин.данных
-def get_couple_vec(data_list, elem1pos, elem2pos):
+def a_get_vec(data_list, pos):
+    return data_list[pos]
+def a_get_couple_vec(data_list, elem1pos, elem2pos):
     return [data_list[elem1pos], data_list[elem2pos]]
-def cng_vec_pos(data_list, pos1, pos2):
+def a_cng_vec_pos(data_list, pos1, pos2):
     '''
     меняет местами векторы во входящем list'е данных
     '''
     data_list[pos1], data_list[pos2] \
     = data_list[pos2], data_list[pos1]
     return data_list
-def sum_vec_to1(vec1, vec2): # суммируем векторы в один
+def a_sum_vec_to1(vec1, vec2): # суммируем векторы в один
     return vec_sum(vec1, vec2)
-def div_vec_to1(vec1, vec2): # получаем отношение одного вектора к другому
+def a_div_vec_to1(vec1, vec2): # получаем отношение одного вектора к другому
     return vec_div(vec1, vec2)
+def a_create_new_dimension(data_list, dimension):
+    '''
+    # крепим значения нового пространства напрямую, от начала наших данных
+    >>> data_list = [[1,2,3],
+                [3,4,5],
+                [6,7,8]]
+    >>> dimension = [1,2]
+    >>> a_create_new_dimension(data_list, dimension)
+    [[1,2,3,1], [3,4,5,2], [6,7,8]]
+    >>>
+    '''
+    new_dimension_args_count = len(dimension)
+    for pos, vector in enumerate(data_list):
+        if pos >= new_dimension_args_count:
+            break
+        data_list[pos] = vector + list(dimension[pos])
+def a_create_new_dimension_data_reversed(data_list, dimension):
+    '''
+    # крепим значения нового пространства напрямую, 
+    # от начала наших данных, которые развернуты
+    >>> data_list = [[1,2,3],
+                [3,4,5],
+                [6,7,8]]
+    >>> dimension = [1,2]
+    >>> a_create_new_dimension_data_reversed(data_list, dimension)
+    [[6,7,8,1], [3,4,5,2], [1,2,3]]
+    >>>
+    '''
+def a_create_new_dimension_all_reversed(data_list, dimension):
+    '''
+    # крепим значения нового развернутого пространства, 
+    # от начала наших развернутых наоборот данных
+    >>> data_list = [[1,2,3],
+                [3,4,5],
+                [6,7,8]]
+    >>> dimension = [1,2]
+    >>> a_create_new_dimension_data_reversed(data_list, dimension)
+    [[6,7,8,2], [3,4,5,1], [1,2,3]]
+    >>>
+    '''
